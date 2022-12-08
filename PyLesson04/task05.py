41cc6792e9953f57713912b96a9b0d1f358c82c9
unwar193 <varan193@mail.ru>
2022-12-08 05:30:16 +0300

    Homework04

---
+++ PyLesson04/task05.py
@@ -0,0 +1,18 @@
+"""5) Реализовать формирование списка, используя функцию range() и возможности
+генератора. В список должны войти четные числа от 100 до 1000
+(включая границы). Необходимо получить результат вычисления произведения всех
+элементов списка.
+Подсказка: использовать функцию reduce()."""
+from functools import reduce
+
+even_list = []
+even_list_gen = [even_list.append(el) for el in range(100, 1001) if
+                 el % 2 == 0]
+print(even_list)
+
+
+def my_func(prev_el, el):
+    return prev_el * el
+
+
+print(reduce(my_func, even_list))
