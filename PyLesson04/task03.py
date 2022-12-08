1cc4d14ac6c670cf99ea7b6c0c019a378bc7cdef
unwar193 <varan193@mail.ru>
2022-12-08 14:23:58 +0300

    Homework04_repair

--- PyLesson04/task03.py
+++ PyLesson04/task03.py
@@ -6,4 +6,10 @@
 #     if el % 20 == 0 or el % 21 == 0:
 #         print(el)
 
-new_list = [print(el) for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]
+# [print(el) for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]
+
+# print(list.append(el) for el in range(20, 241) if el % 20 == 0 or el % 21 == 0)
+
+# print([el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0])
+
+print(list([i for i in range(20, 241) if i % (20 or 21) == 0]))
--- PyLesson04/task05.py
+++ PyLesson04/task05.py
@@ -5,14 +5,16 @@
 Подсказка: использовать функцию reduce()."""
 from functools import reduce
 
-even_list = []
-even_list_gen = [even_list.append(el) for el in range(100, 1001) if
-                 el % 2 == 0]
-print(even_list)
+# even_list = []
+# even_list_gen = [even_list.append(el) for el in range(100, 1001) if
+#                  el % 2 == 0]
+
+# print(even_list)
+print([el for el in range(100, 1001) if el % 2 == 0])
 
 
 def my_func(prev_el, el):
     return prev_el * el
 
 
-print(reduce(my_func, even_list))
+print(reduce(my_func, [el for el in range(100, 1001) if el % 2 == 0]))
